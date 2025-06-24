a8ef5c864711a4f08b021e78abf854578bee5014
Limbureuben <limbureuben4@gmail.com>
2025-06-25 02:35:59 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './views/landing/Homepage'
+import Registration from './views/shared/registration'
 
 function App() {
   
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path='/homepage' element={<Homepage />} />
+        <Route path='/' element={ <Registration />} />
       </Routes>
     </Router>
   )
