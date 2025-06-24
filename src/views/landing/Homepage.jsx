ca9a61bdcef0ab465da0ec41e5a545260e8cc689
Limbureuben <limbureuben4@gmail.com>
2025-06-25 01:42:54 +0300

    changes

---
+++ src/views/landing/Homepage.jsx
@@ -0,0 +1,13 @@
+// pages/HomePage.jsx
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the First Page!</h1>
+      <p>This is the homepage of your React app.</p>
+    </div>
+  );
+}
+
+export default HomePage;
