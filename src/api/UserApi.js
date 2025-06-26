1931321a194571e994d533fa95174699d0169984
Limbureuben <limbureuben4@gmail.com>
2025-06-26 13:32:32 +0300

    changes

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,1 +1,11 @@
-import { React } from 'react'
+const BASE_URL = 'http://localhost:4000/api';
+
+export async function registerUser(data) {
+    const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+
+    return res.json();
+}
