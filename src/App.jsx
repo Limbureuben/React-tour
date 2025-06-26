138f6d233ff7412d493d2976d801d6ff425b0179
Limbureuben <limbureuben4@gmail.com>
2025-06-26 14:29:17 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Registration from './views/shared/registration'
 import Login from './views/shared/login'
 import Footer from './views/layout/footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   
   return (
     <Router>
       <Routes>
+        <ToastContainer position="top-right" autoClose={3000} />
         <Route path='/register' element={ <Registration />} />
         <Route path='/' element={ <Login />} />
         <Route path='/footer' element={<Footer />} />
