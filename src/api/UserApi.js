502c3f3260c2212f2de421e489394599f2be374d
Limbureuben <limbureuben4@gmail.com>
2025-06-26 15:38:43 +0300

    changes

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = 'http://localhost:4000';
 
 export async function registerUser(data) {
-    const res = await fetch(`${BASE_URL}/auth/register`, {
+    const res = await fetch(`${BASE_URL}/api/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
