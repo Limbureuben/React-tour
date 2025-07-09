51e648e59eebe86b1c90ea6ee71bcb2aa41ed4a7
Limbureuben <limbureuben4@gmail.com>
2025-07-09 11:47:39 +0300

    changes

--- src/views/admin/AdminHeader.jsx
+++ src/views/admin/AdminHeader.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+from 'react-icon'
 
 function AdminHeader() {
   return (
-    <div>AdminHeader</div>
+    <header className='header'>
+      <div className='menu-icon'>
+        <BsJustify className='icon'/>
+      </div>
+      <div className='header-left'>
+        <BsSearch className='icon' />
+      </div>
+      <div className='header-right'>
+        <BsFillBellFill className='icon'/>
+        <BsFillEnvelopeFill className='icon'/>
+        <BsPersonCircle  className='icon'/>
+      </div>
+    </header>
   )
 }
 
