a2e7f250b4fee1f50ca342c78984b63cc23989ab
Limbureuben <limbureuben4@gmail.com>
2025-07-10 21:48:37 +0300

    changes

--- src/views/admin/AdminHeader.jsx
+++ src/views/admin/AdminHeader.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-          STORE ADMIN
+          ADMIN STORE
         </Typography>
         <IconButton color="inherit">
           <Badge badgeContent={56} color="error">
